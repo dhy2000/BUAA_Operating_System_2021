2f3ec02a8e65d1aee20deac84221be325b0f545b
10.210.0.67 <10.210.0.67@example.com>
2021-03-16 15:05:45 +0800

    Judgement for lab0-Extra at 2021-03-16T15:05:45+0800

--- ext_examA/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# ^^^^^^ Extra TASK 2 ^^^^^^
-sed -n '8p' $1 | ./programA > outputA
-# ^^^^^^ TASK 2 DONE ^^^^^^
-
--- ext_examB/findx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# ^^^^^^ EXTRA TASK 3 ^^^^^^
-xfile=`find test_dir/ | grep xfile`
-sed 's/char/int/g' ${xfile} > output
-# ^^^^^^ EXTRA TASK 3 DONE ^^^^^^
-
