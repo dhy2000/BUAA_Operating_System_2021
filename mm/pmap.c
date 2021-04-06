7dfc829e6ac84f7d706f44d88322d5aa612688eb
18375354 <18375354@buaa.edu.cn>
2021-04-06 14:29:52 +0800

    test assertion

--- mm/pmap.c
+++ mm/pmap.c
@@ -256,7 +256,9 @@ page_alloc2(struct Page **pp)
     ppn = page2ppn(ppage_temp);
     pa = page2pa(ppage_temp);
 
-    printf("page number is %x, start from pa %x\n", ppn, pa);
+    // printf("page number is %x, start from pa %x\n", ppn, pa);
+    
+    printf("test_fail\n");
 
     return 0;
 }
