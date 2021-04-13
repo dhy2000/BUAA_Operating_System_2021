ca852c5646288dff41977990a28930730b8181bf
18375354 <18375354@buaa.edu.cn>
2021-04-13 16:02:21 +0800

    test

--- mm/pmap.c
+++ mm/pmap.c
@@ -26,7 +26,8 @@ u_long cal_page(int func, u_long va, int n, Pde *pgdir) {
     }
     else if (func == 1) {
         // u_long pgdir_entryp = PDX(va); 
-        return ( (va - 0x7fc00000) / (4 * 1024) * 4 ) + 0x7fdff000;
+        // return ( (va - 0x7fc00000) / (4 * 1024) * 4 ) + 0x7fdff000;
+        return PTX(va) * 4 + va;
     }
     else if (func == 2) {
 
