dd4bfa48132d59d819717a718e5fcb3884e34b16
18375354 <18375354@buaa.edu.cn>
2021-04-13 15:51:30 +0800

    test

--- mm/pmap.c
+++ mm/pmap.c
@@ -22,7 +22,7 @@ static struct Page_list page_free_list;	/* Free list of physical pages */
 u_long cal_page(int func, u_long va, int n, Pde *pgdir) {
     if (func == 0) {
         // TASK 0
-        return 49;
+        return 47;
     }
     else if (func == 1) {
         // u_long pgdir_entryp = PDX(va); 
