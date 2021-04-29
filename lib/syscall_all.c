08213b10b6ddf0757ac5fd1f69ccc38d783bf7af
18375354 <18375354@buaa.edu.cn>
2021-04-29 13:42:25 +0800

    hhh

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -8,6 +8,10 @@
 extern char *KERNEL_SP;
 extern struct Env *curenv;
 
+#define RET_FAIL(r) if (r < 0) return r;
+
+
+
 /* Overview:
  * 	This function is used to print a character on screen.
  * 
@@ -143,7 +147,23 @@ int sys_mem_alloc(int sysno, u_int envid, u_int va, u_int perm)
 	struct Page *ppage;
 	int ret;
 	ret = 0;
-
+    
+    if (va >= UTOP || (perm & PTE_COW)) {
+        return -E_INVAL;
+    }
+    ret = envid2env(envid, &env, 1);
+    if (ret < 0) {
+        return ret;
+    }
+    ret = page_alloc(&ppage);
+    if (ret < 0) {
+        return ret;
+    }
+    ret = page_insert(env->env_pgdir, ppage, va, perm);
+    if (ret < 0) {
+        return ret;
+    }
+    return 0;
 }
 
 /* Overview:
@@ -176,7 +196,26 @@ int sys_mem_map(int sysno, u_int srcid, u_int srcva, u_int dstid, u_int dstva,
 	round_dstva = ROUNDDOWN(dstva, BY2PG);
 
     //your code here
+    if (srcva >= UTOP || dstva >= UTOP) {
+        return -E_INVAL;
+    }
 
+    ret = envid2env(srcid, &srcenv, 1);
+    RET_FAIL(ret)
+    ret = envid2env(dstid, &dstenv, 1);
+    RET_FAIL(ret)
+    ppage = page_lookup(srcenv->env_pgdir, round_srcva, &ppte);
+    if (ppage == NULL) {
+        return -E_INVAL;
+    }
+    if ( (!(*ppte & PTE_R)) && (!!(perm & PTE_R))) {
+        return -E_INVAL;
+    }
+    ppage = pa2page(PTE_ADDR(*ppte));
+    ret = page_insert(dstenv->env_pgdir, ppage, round_dstva, perm);
+    RET_FAIL(ret)
+    
+    ret = 0;
 	return ret;
 }
 
@@ -195,7 +234,13 @@ int sys_mem_unmap(int sysno, u_int envid, u_int va)
 	// Your code here.
 	int ret;
 	struct Env *env;
-
+    
+    if (va >= UTOP) {
+        return -E_INVAL;
+    }
+    ret = envid2env(envid, &env, 1);
+    RET_FAIL(ret)
+    page_remove(env->env_pgdir, va);
 	return ret;
 	//	panic("sys_mem_unmap not implemented");
 }
