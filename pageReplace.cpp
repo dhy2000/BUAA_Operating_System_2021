0609344fa19553f876ecd396453b7a4d3d0ea804
18375354 <18375354@buaa.edu.cn>
2021-05-08 17:59:54 +0800

    fuck

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,76 +1,94 @@
 #include "pageReplace.h"
-#include <cstdio>
 #include <cassert>
 #include <cstddef>
+#include <unordered_map>
+
+#define DEBUG_MESSAGE
+
+#ifdef DEBUG_MESSAGE
+#include <cstdio>
+#endif
 
 #define MAX_PHY_PAGE 64
 #define PAGE_SHIFT 12
-#define TOTAL_PAGE (1 << 19)
 
 #define get_Page(x) ((x)>>PAGE_SHIFT)
 typedef unsigned int u_int;
 
+using std::unordered_map;
+
 struct HNode {
     bool valid;
-    u_int offset;
-    struct HNode *prev, *next;
+    u_int p_offset;
+    struct HNode *prev;
+    struct HNode *next;
 };
-HNode nodes[TOTAL_PAGE];
+
+HNode nodes[MAX_PHY_PAGE];
 extern HNode ntail;
-HNode nhead = {.valid = false, .offset = 0, .prev = NULL, .next = &ntail};
-HNode ntail = {.valid = false, .offset = 0, .prev = &nhead, .next = NULL};
-#define Lhead (nhead.next)
-#define Ltail (ntail.prev)
+HNode nhead = {.valid = 0, .p_offset = 0, .prev = NULL, .next = &ntail};
+HNode ntail = {.valid = 0, .p_offset = 0, .prev = &nhead, .next = NULL};
+unsigned char ncnt = 0;
+#define GET_NEW_NODE (&nodes[ncnt++]) // must ensure ncnt < MAX_PHY_PAGE
+
+unordered_map<u_int, HNode*> pgframe;
 
 inline void List_Insert_Head(HNode *hnode) {
-    // offset is ready
-    hnode->valid = 1;
-    hnode->next = Lhead;
+    hnode->next = nhead.next;
     hnode->prev = &nhead;
-    Lhead->prev = hnode;
-    Lhead = hnode;
+    nhead.next->prev = hnode;
+    nhead.next = hnode;
 }
 
 inline void List_Remove(HNode *hnode) {
-    assert(hnode->valid);
     hnode->next->prev = hnode->prev;
     hnode->prev->next = hnode->next;
-    hnode->valid = 0;
-    // hnode->offset = 0;
 }
 
-inline bool Page_Visit(u_int pg) {
-    HNode *hnode = &nodes[pg];
-    if (hnode->valid) {
-        if (hnode->prev != &nhead) {
-            List_Remove(hnode);
-            List_Insert_Head(hnode);
-        }
-        return true;
-    } else {
-        return false;
-    }
+inline void List_Visit(HNode *hnode) {
+    List_Remove(hnode);
+    List_Insert_Head(hnode);
+}
+
+inline HNode * List_Replace() {
+    HNode *hnode = ntail.prev;
+    List_Remove(hnode);
+    List_Insert_Head(hnode);
+    return hnode;
 }
 
 void pageReplace(long * physic_memory, long nwAdd) {
-    static int count = 0;
     u_int pg = get_Page(nwAdd);
-    // fprintf(stderr, "Addr %d, Page %d\n", nwAdd, pg);
-    if (!Page_Visit(pg)) {
-        if (count < MAX_PHY_PAGE) {
-            physic_memory[count] = pg;
-            nodes[pg].offset = count;
-            List_Insert_Head(&nodes[pg]);
-            count++;
-        } else {
-            // don't need to replace
-            HNode *ndtail = Ltail;
-            assert(ndtail != &nhead);
-            u_int rep_off = ndtail->offset;
-            List_Remove(ndtail);
-            nodes[pg].offset = rep_off;
-            List_Insert_Head(&nodes[pg]);
-            physic_memory[rep_off] = pg;
+    if (pgframe.count(pg)) { // exists
+        HNode *hnode = pgframe[pg];
+        List_Visit(hnode);
+#ifdef DEBUG_MESSAGE
+        fprintf(stderr, "Page Hit: addr=%d, pg=%d, p_off=%d\n", nwAdd, pg, hnode->p_offset);
+#endif
+    } else {
+        if (ncnt < MAX_PHY_PAGE) {
+            // allocate a new page frame
+            physic_memory[ncnt] = pg; // fill in
+            HNode *hnode = GET_NEW_NODE;
+            hnode->p_offset = ncnt - 1;
+            hnode->valid = 1;
+            List_Insert_Head(hnode);
+            pgframe[pg] = hnode;
+#ifdef DEBUG_MESSAGE
+            fprintf(stderr, "Page Miss: addr=%d, pg=%d, p_off=%d (init)\n", nwAdd, pg, hnode->p_offset);
+#endif
+        }
+        else {
+            HNode *hnode = List_Replace();
+            u_int p_offset = hnode->p_offset;
+            u_int pgold = physic_memory[p_offset];
+            assert(pgframe[pgold] == hnode);
+            pgframe.erase(pgold);
+            pgframe[pg] = hnode;
+            physic_memory[p_offset] = pg;
+#ifdef DEBUG_MESSAGE
+            fprintf(stderr, "Page Miss: addr=%d, pg=%d, p_off=%d (replace)\n", nwAdd, pg, hnode->p_offset);
+#endif
         }
     }
 }
