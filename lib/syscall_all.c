a3935b35195e8a158b1e96357e2b38a0983f5966
18375354 <18375354@buaa.edu.cn>
2021-05-08 20:32:12 +0800

    orz

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -201,7 +201,7 @@ int sys_mem_map(int sysno, u_int srcid, u_int srcva, u_int dstid, u_int dstva,
     if (srcva >= UTOP || dstva >= UTOP) {
         return -E_INVAL;
     }
-    if (perm & PTE_V) {
+    if (!(perm & PTE_V)) {
         return -E_INVAL;
     }
 
