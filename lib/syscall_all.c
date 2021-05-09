a79a7a8f251ad9a180d47b144ca49b93b9979d81
18375354 <18375354@buaa.edu.cn>
2021-05-09 21:08:51 +0800

    modify syscall_all.c

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -281,7 +281,7 @@ int sys_env_alloc(void)
     e->env_tf.regs[2] = 0;
     e->env_status = ENV_NOT_RUNNABLE;
     e->env_pri = curenv->env_pri;
-
+    LIST_INSERT_HEAD(&env_sched_list[0], e, env_sched_link);
 	return e->env_id;
 	//	panic("sys_env_alloc not implemented");
 }
@@ -312,10 +312,11 @@ int sys_set_env_status(int sysno, u_int envid, u_int status)
     ret = envid2env(envid, &env, 1);
     if (ret < 0) {return ret;}
     env->env_status = status;
-    if (status == ENV_RUNNABLE) {
+    /*if (status == ENV_RUNNABLE) {
         // printf("## set env(%d) RUNNABLE\n", env->env_id);
         LIST_INSERT_HEAD(&env_sched_list[0], env, env_sched_link);
-    } else if (status == ENV_FREE) {
+    } else*/
+    if (status == ENV_FREE) {
         env_destroy(env);
     }
 	return 0;
