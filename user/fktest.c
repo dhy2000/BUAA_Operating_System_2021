2c1cc38c0b229dc4ba534ee1d5b74dd7e0b7881f
18375354 <18375354@buaa.edu.cn>
2021-05-18 14:56:00 +0800

    modify test prog

--- user/fktest.c
+++ user/fktest.c
@@ -1,5 +1,6 @@
 #include "lib.h"
 
+int global_a;
 
 void umain()
 {
@@ -7,24 +8,26 @@ void umain()
 	int id = 0;
 
 	if ((id = fork()) == 0) {
-		if ((id = fork()) == 0) {
-			a += 3;
-
+		if ((id = tfork()) == 0) {
+			global_a += 3;
+            a += 3;
 			for (;;) {
-				writef("\t\tthis is child2 :a:%d\n", a);
+				writef("\t\tthis is child2 :ga:%d, a:%d\n", global_a, a);
 			}
 		}
 
-		a += 2;
+		global_a += 2;
+        a += 2;
 
 		for (;;) {
-			writef("\tthis is child :a:%d\n", a);
+			writef("\tthis is child :ga:%d, a:%d\n", global_a, a);
 		}
 	}
 
-	a++;
+	global_a++;
+    a++;
 
 	for (;;) {
-		writef("this is father: a:%d\n", a);
+		writef("this is father: ga:%d, a:%d\n", global_a, a);
 	}
 }
