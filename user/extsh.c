eb13e31b2de10b60212ce76fbb0cddab167644c9
18375354 <18375354@buaa.edu.cn>
2021-06-22 20:11:54 +0800

    add command parser for shell

--- user/extsh.c
+++ user/extsh.c
@@ -1,5 +1,6 @@
 #include "lib.h"
 #include "cursor.h"
+#include <args.h>
 
 #define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
 /* ^^^^^^ My Extended Super Shell ^^^^^^^^^ */
@@ -207,6 +208,8 @@ static void special_key_detect() {
 
 /* ^^^^^^ Part 4. Top Interfaces ^^^^^^^^^ */
 
+static void runcmd(char *s); // declaration
+
 /*
  * Overview: callback method when finished entering a command.
  * Pre-Condition: Finished entering a command and hit LF or CR.
@@ -215,10 +218,23 @@ static void special_key_detect() {
 void inputenter() {
     if (input_size > 0) {
         input_buf[input_size] = 0;
-        // TODO: call command parser and runner
-        writef("@@@ command:\"%s\"\n", input_buf);
+        /* writef("@@@ command:\"%s\"\n", input_buf);
         if (strcmp(input_buf, "exit") == 0) {
             exit();
+        } */
+        int r;
+        if ((r = fork()) < 0) 
+            writef("@@ error on fork!\n");
+        else {
+            if (r == 0) { // child
+                runcmd(input_buf);
+                exit();
+                return;
+            } else {
+                // father
+                // TODO: support background command (not wait)
+                wait(r);
+            }
         }
     }
     clearbuffer();
@@ -251,11 +267,240 @@ void mainloop() {
 }
 
 
+/* ^^^^^^ Part 5. Parser for shell ^^^^^^^^^ */
+
+int debug_ = 0;
+int interactive = '?';
+int echocmds = 0;
+
+//
+// get the next token from string s
+// set *p1 to the beginning of the token and
+// *p2 just past the token.
+// return:
+//    0 for end-of-string
+//    > for >
+//    | for |
+//    w for a word
+//
+// eventually (once we parse the space where the nul will go),
+// words get nul-terminated.
+#define WHITESPACE " \t\r\n"
+#define SYMBOLS "<|>&;()"
+
+static int
+_gettoken(char *s, char **p1, char **p2)
+{
+    int t;
+
+    if (s == 0) {
+        //if (debug_ > 1) writef("GETTOKEN NULL\n");
+        return 0;
+    }
+
+    //if (debug_ > 1) writef("GETTOKEN: %s\n", s);
+
+    *p1 = 0;
+    *p2 = 0;
+
+    while(strchr(WHITESPACE, *s))
+        *s++ = 0;
+    if(*s == 0) {
+    //    if (debug_ > 1) writef("EOL\n");
+        return 0;
+    }
+    if(strchr(SYMBOLS, *s)){
+        t = *s;
+        *p1 = s;
+        *s++ = 0;
+        *p2 = s;
+//        if (debug_ > 1) writef("TOK %c\n", t);
+        return t;
+    }
+    *p1 = s;
+    while(*s && !strchr(WHITESPACE SYMBOLS, *s))
+        s++;
+    *p2 = s;
+    if (debug_ > 1) {
+        t = **p2;
+        **p2 = 0;
+//        writef("WORD: %s\n", *p1);
+        **p2 = t;
+    }
+    return 'w';
+}
+
+static int
+gettoken(char *s, char **p1)
+{
+    static int c, nc;
+    static char *np1, *np2;
+
+    if (s) {
+        nc = _gettoken(s, &np1, &np2);
+        return 0;
+    }
+    c = nc;
+    *p1 = np1;
+    nc = _gettoken(np2, &np1, &np2);
+    return c;
+}
+
+#define MAXARGS 16
+void
+runcmd(char *s)
+{
+    char *argv[MAXARGS], *t;
+    int argc, c, i, r, p[2], fd, rightpipe;
+    int fdnum;
+    rightpipe = 0;
+    gettoken(s, 0);
+again:
+    argc = 0;
+    for(;;){
+        c = gettoken(0, &t);
+        switch(c){
+        case 0:
+            goto runit;
+        case 'w':
+            if(argc == MAXARGS){
+                writef("too many arguments\n");
+                exit();
+            }
+            argv[argc++] = t;
+            break;
+        case '<':
+            if(gettoken(0, &t) != 'w'){
+                writef("syntax error: < not followed by word\n");
+                exit();
+            }
+            // Your code here -- open t for reading,
+            // writef("@@@ shell: will open %s for read\n", t);
+            r = open(t, O_RDONLY);
+            if (r < 0) { user_panic("sh.c failed to open file"); }
+            fd = r;
+            dup(fd, 0);
+            close(fd);
+            
+            // dup it onto fd 0, and then close the fd you got.
+            // user_panic("< redirection not implemented");
+            break;
+        case '>':
+            // Your code here -- open t for writing,
+            
+            if(gettoken(0, &t) != 'w'){
+                writef("syntax error: < not followed by word\n");
+                exit();
+            }
+            // writef("@@@ shell: will open %s for write\n", t);
+            r = open(t, O_WRONLY | O_CREAT);
+            if (r < 0) {user_panic("sh.c failed to open file");}
+            fd = r;
+            dup(fd, 1);
+            close(fd);
+            // dup it onto fd 1, and then close the fd you got.
+            // user_panic("> redirection not implemented");
+            break;
+        case '|':
+            // Your code here.
+            //     First, allocate a pipe.
+            //    Then fork.
+            //    the child runs the right side of the pipe:
+            //        dup the read end of the pipe onto 0
+            //        close the read end of the pipe
+            //        close the write end of the pipe
+            //        goto again, to parse the rest of the command line
+            //    the parent runs the left side of the pipe:
+            //        dup the write end of the pipe onto 1
+            //        close the write end of the pipe
+            //        close the read end of the pipe
+            //        set "rightpipe" to the child envid
+            //        goto runit, to execute this piece of the pipeline
+            //            and then wait for the right side to finish
+            // user_panic("| not implemented");
+            
+            
+            pipe(p);
+            if ((rightpipe = fork()) == 0) {
+                dup(p[0], 0);
+                close(p[0]);
+                close(p[1]);
+                goto again;
+            } else {
+                dup(p[1], 1);
+                close(p[1]);
+                close(p[0]);
+                goto runit;
+            }
+
+            break;
+        }
+    }
+
+runit:
+    if(argc == 0) {
+        if (debug_) writef("EMPTY COMMAND\n");
+        return;
+    }
+    argv[argc] = 0;
+    
+    // support halt
+    if (strcmp(argv[0], "halt") == 0 || strcmp(argv[0], "exit") == 0) {
+        syscall_halt();
+    }
+
+
+    if ((r = spawn(argv[0], argv)) < 0)
+        writef("error spawn %s: %e\n", argv[0], r);
+    close_all();
+    if (r >= 0) {
+        if (debug_) writef("[%08x] WAIT %s %08x\n", env->env_id, argv[0], r);
+        wait(r);
+    }
+    if (rightpipe) {
+        if (debug_) writef("[%08x] WAIT right-pipe %08x\n", env->env_id, rightpipe);
+        wait(rightpipe);
+    }
+
+    exit();
+}
+
+void
+usage(void)
+{
+    writef("usage: extsh [-dix]\n");
+    exit();
+}
 
 void umain(int argc, char **argv) {
+
+    int r, interactive, echocmds;
+    interactive = '?';
+    echocmds = 0;
     
     printWelcome();
+     
+    ARGBEGIN{
+    case 'd':
+        debug_++;
+        break;
+    case 'i':
+        interactive = 1;
+        break;
+    case 'x':
+        echocmds = 1;
+        break;
+    default:
+        usage();
+    }ARGEND
+     
+    if(argc > 1)
+        usage();
+    if(interactive == '?')
+        interactive = iscons(0);
+     
     inputenter();
     mainloop();
 
 }
+
