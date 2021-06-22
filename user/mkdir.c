158b3ea700409a3ce49a8658ff28bd9d6c51eab3
18375354 <18375354@buaa.edu.cn>
2021-06-22 23:04:08 +0800

    implemented mkdir

--- user/mkdir.c
+++ user/mkdir.c
@@ -1,8 +1,34 @@
 #include "lib.h"
 #include <args.h>
 
+void mmakedir(const char *path, int recursive) {
+    int r;
+    int mode = recursive ? (FTYPE_DIR | MKDIR_P) : (FTYPE_DIR);
+    r = create(path, mode);
+    if (r < 0) {
+        writef("mkdir failed with code %d\n", r);
+        return;
+    }
+    
+}
+
 void umain(int argc, char **argv) {
-    writef("@ mkdir: argc = %d\n", argc);
-
-
+    // writef("@ mkdir: argc = %d\n", argc);
+    if (argc == 1) {
+        writef("usage: mkdir [-p] dirname\n");
+        return;
+    }
+    if (strcmp(argv[1], "-p") == 0) {
+        if (argc >= 2) {
+            mmakedir(argv[2], 1);
+        } else {
+            writef("error: need dirname");
+        } 
+    } else {
+        if (argv[1][0] == '-') {
+            writef("error: invalid option %s\n", argv[1]);
+        } else {
+            mmakedir(argv[1], 0);
+        }
+    }
 }
