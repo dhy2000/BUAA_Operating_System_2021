10874ffdaef3711b31eb8d66040119562a45877c
18375354 <18375354@buaa.edu.cn>
2021-06-23 01:26:31 +0800

    implemented user touch

--- user/touch.c
+++ user/touch.c
@@ -3,6 +3,13 @@
 
 void umain(int argc, char **argv)
 {
-    writef("@ touch: argc = %d\n", argc);
-
+    // writef("@ touch: argc = %d\n", argc);
+    if (argc < 2) {
+        writef("usage: touch filename");
+    }
+    int r;
+    r = create(argv[1], FTYPE_REG | MKDIR_P);
+    if (r < 0) {
+        writef("touch %s failed with err code %d\n", argv[1], r);
+    }
 }
