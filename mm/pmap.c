818b9a7a73e9c2a91276d0d301cf4828b3815415
18375354 <18375354@buaa.edu.cn>
2021-06-22 20:13:30 +0800

    removed pageout message git add mm/pmap.c

--- mm/pmap.c
+++ mm/pmap.c
@@ -679,6 +679,6 @@ void pageout(int va, int context)
     p->pp_ref++;
 
     page_insert((Pde *)context, p, VA2PFN(va), PTE_R);
-    printf("pageout:\t@@@___0x%x___@@@  ins a page \n", va);
+    // printf("pageout:\t@@@___0x%x___@@@  ins a page \n", va);
 }
 
