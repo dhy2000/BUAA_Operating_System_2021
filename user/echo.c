c9934e703cc8e656ec493066067f2cb124de8574
18375354 <18375354@buaa.edu.cn>
2021-06-24 14:56:32 +0800

    support echo.b display environment variable

--- user/echo.c
+++ user/echo.c
@@ -1,9 +1,12 @@
 #include "lib.h"
 
+char var_value[256];
+
 void
 umain(int argc, char **argv)
 {
     int i, nflag;
+    int r;
 
     nflag = 0;
     if (argc > 1 && strcmp(argv[1], "-n") == 0) {
@@ -13,8 +16,14 @@ umain(int argc, char **argv)
     }   
     for (i = 1; i < argc; i++) {
         if (i > 1)
-            write(1, " ", 1); 
-        write(1, argv[i], strlen(argv[i]));
+            write(1, " ", 1);
+        if (argv[i][0] == '$') {
+            r = user_envvar_get((char*)(argv[i] + 1), var_value);
+            if (r == 0) 
+                write(1, var_value, strlen(var_value));
+        }
+        else
+            write(1, argv[i], strlen(argv[i]));
     }   
     if (!nflag)
         write(1, "\n", 1); 
